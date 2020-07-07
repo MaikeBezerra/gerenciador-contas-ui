3ad4de57540396d096bc4f46d26ccc934043b293
MaikeBezerra <maike_ti@hotmail.com>
2020-07-07 10:19:50 -0300

    Adicionando template de contatos em modulo de pessoa

--- src/app/pessoa/pessoa.module.ts
+++ src/app/pessoa/pessoa.module.ts
@@ -10,6 +10,7 @@ import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { ToastModule } from 'primeng/toast';
 import { DropdownModule } from 'primeng/dropdown';
+import {PanelModule} from 'primeng/panel';
 
 import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
 import { PesquisaPessoasComponent } from './pesquisa-pessoas/pesquisa-pessoas.component';
@@ -33,6 +34,7 @@ import { SharedModule } from '../shared/shared.module';
     TooltipModule,
     ToastModule,
     DropdownModule,
+    PanelModule,
 
     SharedModule,
     PessoasRoutingModule
